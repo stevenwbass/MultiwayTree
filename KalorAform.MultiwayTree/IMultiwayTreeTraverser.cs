9bb66872d90bdcfca3a09ef4400909949e78bd85
Steven Bass <stevenwbass@gmail.com>
2023-02-08 20:40:00 -0500

    add interfaces for dependency injection and behavior customization + add initial breadth-first traversal impl

---
+++ KalorAform.MultiwayTree/IMultiwayTreeTraverser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalorAform.MultiwayTree
+{
+    public interface IMultiwayTreeTraverser
+    {
+        Task<TResult> TraverseAsync<T, TResult>(MultiwayTree<T> tree) where T : IEquatable<T>;
+        Task<TResult> TraverseCumulative<T, TResult>(MultiwayTree<T> tree) where T : IEquatable<T>;
+    }
+}
---
+++ KalorAform.MultiwayTree/INodeVisitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalorAform.MultiwayTree
+{
+    public interface INodeVisitor
+    {
+        public Task<TResult> VisitNodeAsync<T, TResult>(T data);
+        public Task<TResult> VisitNodeCumulativeAsync<T, TResult>(T data, TResult cumulativeResult);
+    }
+}
